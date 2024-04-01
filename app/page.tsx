1940fc82284958f49c218300adf06f7dbde9c8f8
olyshaaa <olgasevel108@gmail.com>
2024-04-01 11:17:11 +0300

    buttons variants

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return(
     <div>
-      <p className='text-green-500 font-bold'>Hello Lingo</p>
-      <Button variant='premium'>click me</Button>
+      
     </div>
   )
 }
