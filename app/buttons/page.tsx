a0378ec2574573cbbfb52b02d26aaed52e2850a1
olyshaaa <olgasevel108@gmail.com>
2024-04-01 11:31:11 +0300

    marketing page

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -3,18 +3,18 @@ import { Button } from '@/components/ui/button'
  const ButtonsPage = () => {
 	return(
 		<div className='p-4 space-y-4 flex flex-col max-w-[200px]'>
-		<Button>default</Button>
-		<Button variant={'primary'}>Primary</Button>
-		<Button variant={'primaryOutline'}>Primary Outline</Button>
-		<Button variant={'secondary'}>Secondary</Button>
-		<Button variant={'secondaryOutline'}>Secondary outline</Button>
-		<Button variant={'danger'}>Danger</Button>
-		<Button variant={'dangerOutline'}>Danger outline</Button>
-		<Button variant={'super'}>Super</Button>
-		<Button variant={'superOutline'}>Super Outline</Button>
-		<Button variant={'ghost'}>Ghost</Button>
-		<Button variant={'sidebar'}>Sidebar</Button>
-		<Button variant={'sidebarOutline'}>Sidebar Outline</Button>
+			<Button>default</Button>
+			<Button variant={'primary'}>Primary</Button>
+			<Button variant={'primaryOutline'}>Primary Outline</Button>
+			<Button variant={'secondary'}>Secondary</Button>
+			<Button variant={'secondaryOutline'}>Secondary outline</Button>
+			<Button variant={'danger'}>Danger</Button>
+			<Button variant={'dangerOutline'}>Danger outline</Button>
+			<Button variant={'super'}>Super</Button>
+			<Button variant={'superOutline'}>Super Outline</Button>
+			<Button variant={'ghost'}>Ghost</Button>
+			<Button variant={'sidebar'}>Sidebar</Button>
+			<Button variant={'sidebarOutline'}>Sidebar Outline</Button>
 		</div>
 	)
  }
